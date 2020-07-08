5362a5845d47ae894102234d5e2fb640e8e9dc95
xApnea <jcbruno150@gmail.com>
2020-07-08 18:19:42 -0400

    Built out crud operations for Updates and Comments tables

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const port = 3001;
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const db = require('../db/Index.js');
+const moment = require('moment');
 
 
 app.use(morgan());
@@ -31,8 +32,11 @@ app.get('/:id', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
+////////////////////////////////////////////////////////////////////////
+// Updates
+////////////////////////////////////////////////////////////////////////
 
-// to do Test the code to see if it works
+// Get Update by DB
 app.get('/api/update/:id', function (req, res) {
   db.Update.findOne({where: {id: req.params.id }})
   .then(function(data){
@@ -40,6 +44,61 @@ app.get('/api/update/:id', function (req, res) {
   })
 });
 
+// Get all updates
+app.get('/api/update/', function (req, res) {
+  db.Update.findAll()
+  .then(function(data){
+    res.status(200).send(data);
+  })
+});
+
+app.post('/api/update/', function(req, res) {
+  db.Update.create({
+    title: req.body.title,
+    author: req.body.author,
+    imageUrl: req.body.imageUrl,
+    createdAt: Date.now(),
+    body: req.body.body,
+    likes: req.body.likes,
+    campaignID: req.body.campaignID
+  })
+  .then((response) => {
+    res.status(200).send(response);
+  })
+  .catch((err) => {
+    res.status(400).send(err);
+  })
+})
+
+app.put('/api/update/:id', function(req, res) {
+  const id = req.params.id;
+  db.Update.update(
+    { title: req.body.title},
+    { where: { id: id } }
+  )
+    .then((result) => {
+      res.status(200).send('Record successfully updated');
+    })
+    .catch(err => {
+      res.status(400).send(err)
+    })
+})
+
+app.delete('/api/update/:id', function(req, res) {
+  const id = req.params.id;
+  db.Update.destroy(
+    { where: { id: id } }
+  )
+    .then((result) => {
+      res.status(200).send('Record successfully deleted');
+    })
+    .catch(err => {
+      res.status(400).send(err)
+    })
+})
+////////////////////////////////////////////////////////////////////////
+// Comments
+////////////////////////////////////////////////////////////////////////
 
 app.get('/api/comment/:id', function (req, res) {
   db.Comment.findAll({where: {updateID: req.params.id }})
@@ -48,13 +107,7 @@ app.get('/api/comment/:id', function (req, res) {
   })
 });
 
-const GetAllUpdates = app.get('/api/update/', function (req, res) {
-  db.Update.findAll()
-  .then(function(data){
-    res.status(200).send(data);
-  })
-});
-
+//Get all comments in db.
 app.get('/api/comment/', function (req, res) {
   db.Comment.findAll()
   .then(function(data){
@@ -74,15 +127,32 @@ app.post('/api/comment/', function (req, res) {
   })
 });
 
+app.put('/api/comment/:id', function(req, res) {
+  const id = req.params.id;
+  db.Update.update(
+    { comment: req.body.comment },
+    { where: {updateID: id} }
+  )
+    .then((result) => {
+      res.status(200).send('Comment successfully updated');
+    })
+    .catch(err => {
+      res.status(400).send(err)
+    })
+})
 
-// app.get(‘*’, (req, res) => {
-//   res.sendFile(path.join(__dirname, ‘../dist’, ‘index.html’));
-// });
-
-
-
-
-// module.exports.GetAllUpdates = GetAllUpdates;
+app.delete('/api/comment/:id', function(req, res) {
+  const id = req.params.id;
+  db.Update.destroy(
+    { where: {updateID: id} }
+  )
+    .then((result) => {
+      res.status(200).send('Comment successfully deleted');
+    })
+    .catch(err => {
+      res.status(400).send(err)
+    })
+})
 
 
 app.listen(port, () => console.log(`listening at http://localhost:${port}`))
