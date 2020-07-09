2360ddc3a8ed88ba96ee2d40ed424eefb1e64f73
xApnea <jcbruno150@gmail.com>
2020-07-08 23:17:23 -0400

    Built out CRUD operations

--- server/index.js
+++ server/index.js
@@ -10,20 +10,10 @@ const db = require('../db/Index.js');
 const moment = require('moment');
 
 
-app.use(morgan());
-// app.use('/:id', express.static(path.join(__dirname, '../dist')));
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-app.use(cors());//http-proxy-middleware instead of cors?
-
-
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   next();
-// });
-
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
@@ -45,12 +35,12 @@ app.get('/api/update/:id', function (req, res) {
 });
 
 // Get all updates
-app.get('/api/update/', function (req, res) {
-  db.Update.findAll()
-  .then(function(data){
-    res.status(200).send(data);
-  })
-});
+// app.get('/api/update/', function (req, res) {
+//   db.Update.findAll()
+//   .then(function(data){
+//     res.status(200).send(data);
+//   })
+// });
 
 app.post('/api/update/', function(req, res) {
   db.Update.create({
@@ -108,12 +98,12 @@ app.get('/api/comment/:id', function (req, res) {
 });
 
 //Get all comments in db.
-app.get('/api/comment/', function (req, res) {
-  db.Comment.findAll()
-  .then(function(data){
-    res.status(200).send(data);
-  })
-});
+// app.get('/api/comment/', function (req, res) {
+//   db.Comment.findAll()
+//   .then(function(data){
+//     res.status(200).send(data);
+//   })
+// });
 
 app.post('/api/comment/', function (req, res) {
   db.Comment.create({
@@ -144,7 +134,7 @@ app.put('/api/comment/:id', function(req, res) {
 app.delete('/api/comment/:id', function(req, res) {
   const id = req.params.id;
   db.Update.destroy(
-    { where: {updateID: id} }
+    { where: {id: id} }
   )
     .then((result) => {
       res.status(200).send('Comment successfully deleted');
