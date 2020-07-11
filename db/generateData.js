6967e9d266ba8fab5e00191b8bb1662f2ee371df
xApnea <jcbruno150@gmail.com>
2020-07-10 20:54:18 -0400

    Generator writes Updates to a csv file

--- db/generateData.js
+++ db/generateData.js
@@ -1,14 +1,19 @@
 const faker = require('faker');
 const fs = require('fs');
+const cliProgress = require('cli-progress');
 
-//const numOfRecords = 1000000;
-const numOfRecords = 1000000;
-const pathName = 'generatedData.json';
+const numOfRecords = 100000;
+
+const pathName = 'generatedData.csv';
+const encoding = 'utf8';
 const csvWriter = fs.createWriteStream(pathName);
 
+//write headers
+csvWriter.write('campaignID,updateID,title,author,imageUrl,createdAt,body,likes\n', encoding);
 
-const generateUpdates = function() {
+const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
+const generateUpdates = function(callback) {
   const createImage = function() {
     var url = 'http://picsum.photos/seed/'
     var urlend='/200/300'
@@ -16,50 +21,57 @@ const generateUpdates = function() {
     return url+randomNumber+urlend;
   }
 
-  var updateID = 1;
+  //Start CLI progress bar
+  bar.start(numOfRecords, 0);
 
-  for(var campaign = 0; campaign < numOfRecords; campaign++) {
-    var numOfUpdates = Math.ceil(Math.random()*5);
-    for (var i = 0; i < numOfUpdates; i++) {
-      var imageUrl = createImage();
+  var id = 0;
+  var campaign = 0;
+  var updates = numOfRecords;
 
-      var update = {
-        campaignID: campaign,
-        updateID: updateID,
-        title: faker.lorem.words(),
-        author: faker.name.findName(),
-        imageUrl: imageUrl,
-        createdAt: faker.date.past(),
-        body: faker.lorem.paragraphs(),
-        likes: faker.random.number()
+  const writeUpdates = function() {
+    var ok = true;
+    while (updates > 0 && ok) {
+      campaign++;
+      var numOfUpdates = 1;
+
+      for (var i = 0; i < numOfUpdates; i++) {
+        updates--;
+        id++;
+
+        const campaignID = campaign;
+        const updateID = id;
+        const title = faker.lorem.words();
+        const author = faker.name.findName();
+        const imageUrl = createImage();
+        const createdAt = faker.date.past();
+        const body = faker.lorem.paragraphs();
+        const likes = faker.random.number();
+
+        var update = `${campaignID},${id},${title},${author},${imageUrl},${createdAt},${body},${likes}\n`
+
+        //if this is the last record to be written, call write with callback
+        if (updates === 0) {
+          bar.increment();
+          csvWriter.write(update, encoding, callback);
+        } else {
+          //otherwise, must check if buffer still has space.
+          bar.increment();
+          ok = csvWriter.write(update, encoding);
+        }
       }
-
-      //updates.push(update)
-      csvWriter.write(JSON.stringify(update));
-
-      // const numOfComments = Math.ceil(Math.random()*10);
-      // for (var j = 0; j < numOfComments; j++) {
-      //   var comment = {
-      //     updateID: updateID,
-      //     userName: faker.name.findName(),
-      //     comment:faker.lorem.sentences(),
-      //     createdAt: faker.date.past(),
-      //   }
-      // }
-
-      updateID++;
     }
-
+    if (updates > 0) {
+      csvWriter.once('drain', writeUpdates);
+    }
   }
-
-  csvWriter.on('finish', () => {
-    console.log(`wrote all the data to file ${pathName}`);
- });
-
-  csvWriter.on('error', function (err) {
-    console.error(err);
-  });
+  writeUpdates();
 };
 
-
-generateUpdates();
+const startTime = new Date().getTime();
+generateUpdates(() => {
+  const endTime = new Date().getTime();
+  const elapsed = (((endTime - startTime) / 1000) / 60).toFixed(3);
+  bar.stop();
+  console.log(`Finished writing all updates to CSV in ${elapsed} minutes`);
+  csvWriter.end()
+});
