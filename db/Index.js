41254f19bed2c4d1b62c69b8a285e86d347bccab
xApnea <jcbruno150@gmail.com>
2020-07-13 19:25:04 -0400

    Refactored models to be associated, and use postgres dialect

--- db/Index.js
+++ db/Index.js
@@ -1,22 +1,8 @@
-// mysql
-// .createConnection({
-//   user: DATABASE_USER,
-//   password: DATABASE_PASSWORD
-// })
-// .then((sqlConnection) => {
-//   sqlConnection
-//     .query(`CREATE DATABASE IF NOT EXISTS ${DATABASE_NAME};`)
-//     .then(() => {
-//       // Connect to sequelize here!
-//      })
-
-
 const { Sequelize, DataTypes, Model } = require('sequelize');
 
-
-const sequelize = new Sequelize('kickstarter', 'root', null, {
-  host: '127.0.0.1',
-  dialect: 'mysql'
+const sequelize = new Sequelize('kickstarter', 'postgres', 'postgres', {
+  //   host: '127.0.0.1',
+  dialect: 'postgres'
 });
 
 sequelize.authenticate()
@@ -25,8 +11,16 @@ sequelize.authenticate()
   })
   .catch(function(errors) {console.log('Unable to connect to the database:', errors)})
 
-class Update extends Model {}
-Update.init({
+const Update = sequelize.define('Update', {
+  campaignID: {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
+  updateID: {
+    type: DataTypes.INTEGER,
+    unique: true,
+    allowNull: true
+  },
   title: {
     type: DataTypes.STRING,
     allowNull: false
@@ -51,18 +45,12 @@ Update.init({
     type: DataTypes.INTEGER,
     allowNull: false
   },
-  campaignID: {
-    type: DataTypes.INTEGER,
-    allowNull: true
-  },
 }, {
   sequelize,
   modelName: 'Update',
 });
 
 const Comment = sequelize.define('Comment', {
-  // Model attributes are defined here
-
   updateID: {
     type: DataTypes.INTEGER,
     allowNull: true
@@ -83,6 +71,13 @@ const Comment = sequelize.define('Comment', {
   sequelize,
   modelName: 'Comment',
 });
+
+Update.associate = () => {
+  Update.hasMany(Comment, {foreignKey: 'updateID'})
+}
+Comment.associate = () => {
+  Comment.belongsTo(Update)
+}
 
 sequelize.sync();
 
